f986bc5446e781a14e5716b00bdee99f15aa3ab7
Tanuj22 <agarwal.5@iitj.ac.in>
2022-04-28 23:27:21 +0530

    flask post request for crop reco
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- backend/crop_recomendation/prediction.py
+++ backend/crop_recomendation/prediction.py
@@ -2,6 +2,8 @@ import pickle
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from flask import Blueprint
+from flask import request
+
 
 recomendation_api = Blueprint('recomendation_api', __name__)
 
@@ -11,8 +13,20 @@ with open('crop_recomendation/RandomForest.pkl', 'rb') as f:
     RF = pickle.load(f)
 
 
-@recomendation_api.route('/predict')
+# data = np.array([[83, 45, 60, 28, 70.3, 7.0, 150.9]])
+
+
+
+@recomendation_api.route('/predict', methods=['POST'])
 def predict():
-    data = np.array([[83, 45, 60, 28, 70.3, 7.0, 150.9]])
+    N = request.json['N']
+    P = request.json['P']
+    K = request.json['K']
+    temperature = request.json['temperature']
+    humidity = request.json['humidity']
+    ph = request.json['ph']
+    rainfall = request.json['rainfall']
+    
+    data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
     prediction = RF.predict(data)
     return prediction[0]
